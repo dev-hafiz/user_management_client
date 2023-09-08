cb93d9aacd5b4b973522a6289017a500c01f83f3
okshi <johnstarhafiz@gmail.com>
2023-09-08 23:15:22 +0600

    Complete initial refector user management client site code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "./components/router/routes/routes";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={routes} />
-    </>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
